79c061a1e5acee6680003a44bc6395652a6dbc9a
elsarodriguezt <100299303+elsarodriguezt@users.noreply.github.com>
2022-03-09 16:44:38 +0100

    Update puzzle1.rb

--- puzzle1.rb
+++ puzzle1.rb
@@ -10,17 +10,17 @@ class Rfid
 			begin
 				@@r.picc_request(MFRC522::PICC_REQA)  #Establiment de perifèric
 				uid_dec, a = @@r.picc_select          #Llegim. Si expira el timeout llançem l'error a la següent línia
-			rescue CommunicationError => e            #Timeout			           
-			else					   				  #Ja tenim el uid
+			rescue CommunicationError => e                #Timeout			           
+			else					      #Ja tenim el uid
 				aux = 0			   
 			end
 		end
-		hex = Array.new				           		  #Vector hexadecimal
+		hex = Array.new				              #Vector hexadecimal
 		uid_dec.length.times do |i|
 			hex[i]=uid_dec[i].to_s(16)
 		end
 		
-		return hex.join().upcase			   		  #Retornem uid capturat en forma de string concatenat, en majúscules
+		return hex.join().upcase			      #Retornem uid capturat en forma de string concatenat, en majúscules
 	end
 end
 
